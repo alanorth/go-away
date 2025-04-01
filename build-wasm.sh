8ed0f69a19dbe7f221c6398c7ac994bd754dc21c
WeebDataHoarder <57538841+WeebDataHoarder@users.noreply.github.com>
2025-04-01 07:04:47 +0200

    Bundle wasm file to prevent full Clang and LLVM build

--- build-wasm.sh
+++ build-wasm.sh
@@ -30,12 +30,3 @@ export PATH="$PATH:$(realpath ./build/release/tinygo/bin/)"
 popd
 
 go generate ./...
-
-do_compress () {
-  find "$1" \( -type f -name "*.wasm" -o -name "*.css" -o -name "*.js" -o -name "*.mjs" \) -exec zopfli {} \;
-  find "$1" \( -type f -name "*.wasm" -o -name "*.css" -o -name "*.js" -o -name "*.mjs" \) -exec brotli -v -f -9 -o {}.br {} \;
-  #find "$1" \( -type f -name "*.wasm" -o -name "*.css" -o -name "*.js" -o -name "*.mjs" \) -exec zstd -v -f -19 -o {}.zst {} \;
-}
-
-do_compress challenge/
-do_compress assets/
